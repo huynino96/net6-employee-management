185215da2273f8d9d16c31f35e1ecc7cf7a5c560
Huy Ho <huynino0496@gmail.com>
2022-09-18 11:17:00 +0700

    Added CRUD table

---
+++ EmployeeManagement/EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EmployeeManagement.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeContext _context;
+
+        //Constructor
+        public EmployeeController(EmployeeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Employees.ToListAsync());
+        }
+
+        // GET: Employee/AddOrEdit
+        public IActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+            {
+                return View(new Employee());
+            }
+            else
+            {
+                return View(_context.Employees.Find(id));
+            }
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("EmployeeId, FullName, EmpCode, Position, OfficeLocation")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                if (employee.EmployeeId == 0)
+                {
+                    _context.Add(employee);
+                }
+                else
+                {
+                    _context.Update(employee);
+                }
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        //GET: Employee/Delete/{id}
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
+
--- EmployeeManagement/EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/EmployeeManagement/Models/Employee.cs
@@ -8,14 +8,14 @@ namespace EmployeeManagement.Models
     public class Employee
     {
         [Key]
-        public int EmployeeID { get; set; }
+        public int EmployeeId { get; set; }
 
-        [Column(TypeName = "navchar(250)")]
-        [Required(ErrorMessage = "This is required field")]
+        [Column(TypeName = "nvarchar(250)")]
+        [Required(ErrorMessage = "This field is required.")]
         [DisplayName("Full Name")]
         public string FullName { get; set; }
 
-        [Column(TypeName = "varchar(10")]
+        [Column(TypeName = "varchar(10)")]
         [DisplayName("Emp. Code")]
         public string EmpCode { get; set; }
 
