2bc0125d0d5be1ebeffdffc23c2082f860ba6bc8
Huy Ho <huynino0496@gmail.com>
2022-09-18 10:26:35 +0700

    Initial Project

---
+++ EmployeeManagement/EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+
+        [Column(TypeName = "navchar(250)")]
+        [Required(ErrorMessage = "This is required field")]
+        [DisplayName("Full Name")]
+        public string FullName { get; set; }
+
+        [Column(TypeName = "varchar(10")]
+        [DisplayName("Emp. Code")]
+        public string EmpCode { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public string Position { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        [DisplayName("Office Location")]
+        public string OfficeLocation { get; set; }
+    }
+}
+
